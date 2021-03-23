b2e3816b2d3624faa818b6984273c9d3eb281ce4
zznixt07 <zznixt07@protonmail.com>
2021-03-23 23:06:43 +0545

    added web audio player

--- frontend_music_player_web/assets/js/player.js
+++ frontend_music_player_web/assets/js/player.js
@@ -14,17 +14,51 @@ const ap = new APlayer({
             artist: '20syl',
             url: './assets/audio/1.mp3',
         },
-    ]
+    ],
 });
 
-const btn = document.getElementById('button')
-btn.addEventListener('click', function () {
-    ap.list.add([
-        {
-            name: 'walking on the moon',
-            artist: 'Computer Glitch',
-            url: './assets/audio/walking.webm',
-        },
-    ]);
+
+const audioElem = document.querySelector('audio')
+const playPauseBtn = document.querySelector('.play-or-pause-track')
+let paused = true
+playPauseBtn.addEventListener('click', function () {
+    // audioElem.play()
+    if (paused) {
+        ap.play()
+        paused = false
+    }
+    else {
+        ap.pause()
+        paused = true
+    }
 
 })
+
+ap.on('play', function () {
+    playPauseBtn.textContent = '||'
+})
+
+ap.on('pause', function () {
+    playPauseBtn.textContent = '|>'
+})
+
+document.querySelector('.aplayer .aplayer-music').insertAdjacentHTML(
+    'beforeend',
+    `<div class="controls-btns">
+        <button class="prev-track">Prev</button>
+        <button class="play-or-pause-track">|></button>
+        <button class="next-track">Next</button>
+    </div>`
+)
+
+
+// const btn = document.getElementById('button')
+// btn.addEventListener('click', function () {
+//     ap.list.add([
+//         {
+//             name: 'walking on the moon',
+//             artist: 'Computer Glitch',
+//             url: './assets/audio/walking.webm',
+//         },
+//     ]);
+// })
