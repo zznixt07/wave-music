f891239f78844ace746c132f49a6a4788582e33e
zznixt07 <zznixt07@protonmail.com>
2021-04-05 21:39:10 +0545

    Usersignup modelform

---
+++ web_music_player/account/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from user.models import Userbase, Profile
+
+class UserSignupForm(forms.ModelForm):
+    accept_t_and_c = forms.BooleanField(label='',required=True)
+    class Meta:
+        model = Profile
+        fields = [
+            'username', 'password', 'first_name', 'last_name',
+            'email', 'country', 'gender', 'age',
+        ]
+        widgets = {
+            'email': forms.TextInput(attrs={'placeholder': 'E-mail Address'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'password': forms.TextInput(attrs={
+                'placeholder': 'Password', 'type': 'password'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+        }
